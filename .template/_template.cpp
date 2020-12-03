39b073f5e61793aa74de646271853ae961fe2dc0
Ralf Holly <ralf.holly.ext@zeiss.comm>
2020-12-03 11:30:33 +0100

    Updated template to C++17

--- .template/_template.cpp
+++ .template/_template.cpp
@@ -1,25 +1,17 @@
-#include <iostream>
-#include <string>
-#include <vector>
-#include <map>
-#include <cstring>
-#include <cstdio>
+#include <bits/stdc++.h>
 using namespace std;
 
 #include "gmock/gmock.h"
 using namespace testing;
 
-
 namespace testing {
 namespace _template {
-
 
 TEST(_template, YourTest) {
     EXPECT_EQ(3, 1 + 2);
     cout << "Hello World!" << endl;
     // Your code here...
 }
-
 
 } // namespace _template
 } // namespace testing
